24b4ee97738ed9b8e348b1d50924a66be8e72cc3
MMaddoxx <87891744+MMaddoxx@users.noreply.github.com>
2022-04-17 21:30:58 +0700

    Update
    Fix Buzzer

--- Arduino.ino
+++ Arduino.ino
@@ -4,9 +4,9 @@
 void setup() {
   Serial.begin(9600);
   pinMode(buzzer, OUTPUT);
-  pinMode(switch,INPUT_PULLUP);
-  //xTaskCreate(ReadHeartrate, "Heart Rate", 100, NULL, 1, NULL);
-  xTaskCreate(ReadBreathing, "Breathing", 100, NULL, 1, NULL);
+  pinMode(switch, INPUT_PULLUP);
+  xTaskCreate(ReadHeartrate, "Heart Rate", 128, NULL, 1, NULL);
+  xTaskCreate(ReadBreathing, "Breathing", 128, NULL, 1, NULL);
 
 }
 
@@ -51,18 +51,14 @@ void ReadBreathing(void *) {
     }
     if (millis() - timer >= 5000)
     {
-      BuzzerAlert();
-      Serial.println("BuzzerAlert");
-      timer=millis();
+      while (digitalRead(switch) == HIGH)
+      {
+        tone(buzzer, 1000);
+      }
+      noTone(buzzer);
+      timer = millis();
     }
 
     //Serial.println(Breathingrate);
   }
-}
-void BuzzerAlert() {
-  while (digitalRead(switch) == HIGH)
-  {
-    tone(buzzer, 1000);
-  }
-   noTone(buzzer);
 }
