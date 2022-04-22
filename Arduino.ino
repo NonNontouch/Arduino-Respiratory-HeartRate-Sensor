f103d43b01fce26c436bd8858cc3345cd3add4dc
Narongpol Kijrangsan <28519551+nkd3v@users.noreply.github.com>
2022-04-22 21:23:58 +0700

    Fix bugs

--- Arduino.ino
+++ Arduino.ino
@@ -1,64 +1,92 @@
 #include <Arduino_FreeRTOS.h>
-#define buzzer 9
-#define switch 10
+
+bool alerting = false;
+unsigned long timer = 0;
+
 void setup() {
   Serial.begin(9600);
-  pinMode(buzzer, OUTPUT);
-  pinMode(switch, INPUT_PULLUP);
-  xTaskCreate(ReadHeartrate, "Heart Rate", 128, NULL, 1, NULL);
-  xTaskCreate(ReadBreathing, "Breathing", 128, NULL, 1, NULL);
 
+  xTaskCreate(readBreathingRate, "Breathing Rate", 128, NULL, 1, NULL);
+  xTaskCreate(buzzerAlert, "Buzzer Alert", 128, NULL, 1, NULL);
+  xTaskCreate(readSwitch, "Switch", 128, NULL, 1, NULL);
 }
 
-void loop() {
+void loop() {}
 
+void readBreathingRate(void *pvParameters) {
+  const int breathingPin = A1;
+  pinMode(breathingPin, INPUT);
 
-}
-void ReadHeartrate() {
-  while (1)
-  {
-    delay(20);
-    int Heartrate = analogRead(A2);
-    //Serial.println(Heartrate);
-  }
+  int minnum = 500, maxnum = 0, avg;
 
-}
-void ReadBreathing(void *) {
-  int minnum = 500, maxnum = 0, avg, Breathingrate;
-  int timer;
-  for (int i = 0; i < 50; i++)
-  {
-    int reading = analogRead(A1);
-    if (reading < minnum)
-    {
+  for (int i = 0; i < 50; i++) {
+    int reading = analogRead(breathingPin);
+
+    if (reading < minnum) {
       minnum = reading;
     }
-    if (reading > maxnum)
-    {
+    if (reading > maxnum) {
       maxnum = reading;
     }
-    delay(100);
+
+    vTaskDelay(100 / portTICK_PERIOD_MS);
   }
   avg = (minnum + maxnum) / 2;
 
-  while (1)
-  {
-    delay(20);
-    Breathingrate = analogRead(A1);
-    if ((Breathingrate >= avg && Breathingrate <= avg + 50) || (Breathingrate <= avg && Breathingrate >= avg - 50))
-    {
-      timer = millis();
-    }
-    if (millis() - timer >= 5000)
-    {
-      while (digitalRead(switch) == HIGH)
-      {
-        tone(buzzer, 1000);
-      }
-      noTone(buzzer);
+  while (1) {
+    int breathingRate = analogRead(breathingPin);
+
+    if ((breathingRate >= avg && breathingRate <= avg + 50) || (breathingRate <= avg && breathingRate >= avg - 50)) {
       timer = millis();
     }
 
-    //Serial.println(Breathingrate);
+    if (millis() - timer >= 5000) {
+      alerting = true;
+    }
+
+    Serial.println(breathingRate + String(", ") + avg);
+
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+}
+
+void buzzerAlert(void *pvParameters) {
+  const int buzzerPin = 9;
+  pinMode(buzzerPin, OUTPUT);
+
+  while (1) {
+    while (alerting) {
+      digitalWrite(buzzerPin, HIGH);
+      vTaskDelay(200 / portTICK_PERIOD_MS);
+      digitalWrite(buzzerPin, LOW);
+      vTaskDelay(200 / portTICK_PERIOD_MS);
+    }
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void readSwitch(void *pvParameters) {
+  const int swPin = 10;
+  pinMode(swPin, INPUT_PULLUP);
+
+  const int ledPin = 3;
+  pinMode(ledPin, OUTPUT);
+
+  bool reading = HIGH;
+  bool lastState = HIGH;
+
+  while (1) {
+    reading = digitalRead(swPin);
+    if (reading != lastState) {
+      if (reading == LOW) {
+        alerting = false;
+        timer = millis();
+        digitalWrite(ledPin, alerting);
+      }
+    }
+    lastState = reading;
+
+    vTaskDelay(100 / portTICK_PERIOD_MS);
   }
 }
